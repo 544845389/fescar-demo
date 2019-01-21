d16814db3ecccf83bda4c311af8f2ecc3cb2b0c3
houcunlu <544845389@qq.com>
2019-01-21 13:53:11 +0800

    调整

--- server-pay/src/main/java/com/example/ServerPayApplication.java
+++ server-pay/src/main/java/com/example/ServerPayApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 @SpringBootApplication
